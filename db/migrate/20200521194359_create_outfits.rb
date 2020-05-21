7f885b13ce1f294a0198bace9bb62b67756e6538
gigi <mmgamao1@gmail.com>
2020-05-21 15:29:07 -0700

    added seed data and edited migration file for outfits

--- db/migrate/20200521194359_create_outfits.rb
+++ db/migrate/20200521194359_create_outfits.rb
@@ -3,7 +3,7 @@ class CreateOutfits < ActiveRecord::Migration[6.0]
     create_table :outfits do |t|
       t.string :name
       t.string :description
-
+      t.bigint :weather_id
       t.timestamps
     end
   end
