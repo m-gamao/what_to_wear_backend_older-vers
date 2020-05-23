0b5b63c429902c368626d626507c68d62f5099e0
gigi <mmgamao1@gmail.com>
2020-05-22 20:43:02 -0700

    created outfit controller and edited seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,43 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-weathers = Weather.create([
-    {name: 'Freezing', temperature: '0-32 degrees'},
-    {name: 'Freezing/Rainy' }, 
+# weathers = Weather.create([
+#     {name: 'Freezing', temperature: '0-32 degrees'},
+#     {name: 'Freezing/Rainy' }, 
 
-    {name: 'Cold', temperature: '33-50 degrees' }, 
-    {name: 'Cold/Rainy' }, 
-    {name: 'Cold/Snowing' }, 
+#     {name: 'Cold', temperature: '33-50 degrees' }, 
+#     {name: 'Cold/Rainy' }, 
+#     {name: 'Cold/Snowing' }, 
 
-    {name: 'Cool', temperature: '51-69 degrees' }, 
-    {name: 'Cool/Rainy' }, 
+#     {name: 'Cool', temperature: '51-69 degrees' }, 
+#     {name: 'Cool/Rainy' }, 
 
-    {name: 'Warm', temperature: '70-80 degrees' }, 
-    {name: 'Warm/Rainy' }, 
+#     {name: 'Warm', temperature: '70-80 degrees' }, 
+#     {name: 'Warm/Rainy' }, 
 
-    {name: 'Hot', temperature: '81-95 degrees' },
-    {name: 'Hot/Rainy' },
+#     {name: 'Hot', temperature: '81-95 degrees' },
+#     {name: 'Hot/Rainy' },
 
-    {name: 'Scorching', temperature: '96-120 degrees' }])  
+#     {name: 'Scorching', temperature: '96-120 degrees' }])  
+   
     
-    outfits = Outfit.create([
-      {name: 'Freezing outfit 1', weather: weathers[0]},
-      {name: 'Freezing outfit 2', weather: weathers[1]},
-      {name: 'Cold outfit 1', weather: weathers[2]},
-      {name: 'Cold outfit 2', weather: weathers[3]},
-      {name: 'Cold outfit 3', weather: weathers[4]},
-      {name: 'Cool outfit 1', weather: weathers[5]},
-      {name: 'Cool outfit 2', weather: weathers[6]},
-      {name: 'Warm outfit 1', weather: weathers[7]},
-      {name: 'Warm outfit 2', weather: weathers[8]},
-      {name: 'Hot outfit 1', weather: weathers[9]},
-      {name: 'Hot outfit 2', weather: weathers[10]},
-      {name: 'Scorching outfit 1', weather: weathers[11]}]
-    )
+
+
+    # outfits = Outfit.create([
+    #   {name: 'Freezing outfit 1', weather: weathers[0]},
+    #   {name: 'Freezing outfit 2', weather: weathers[1]},
+    #   {name: 'Cold outfit 1', weather: weathers[2]},
+    #   {name: 'Cold outfit 2', weather: weathers[3]},
+    #   {name: 'Cold outfit 3', weather: weathers[4]},
+    #   {name: 'Cool outfit 1', weather: weathers[5]},
+    #   {name: 'Cool outfit 2', weather: weathers[6]},
+    #   {name: 'Warm outfit 1', weather: weathers[7]},
+    #   {name: 'Warm outfit 2', weather: weathers[8]},
+    #   {name: 'Hot weather outfit 1', weather: weathers[9]},
+    #   {name: 'Hot weather outfit 2', weather: weathers[10]},
+    #   {name: 'Scorching 1', weather: weathers[11]}]
+    # )
+
+
+  # TEST:
+  Outfit.create(name: 'Freezing outfit 1')
