7327ddefdb3a23df68cb3399c27fa58b525336ff
gigi <mmgamao1@gmail.com>
2020-05-23 12:41:54 -0700

    routes.rb added weather resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :outfits, only: [:index, :create]
+      resources :weathers, only: [:index]
     end
   end
 end
