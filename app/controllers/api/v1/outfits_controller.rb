b9f17f94e2909bfcdf59923cf9c4510a16713ed4
gigi <mmgamao1@gmail.com>
2020-05-24 12:26:59 -0700

    outfits controller added

---
+++ app/controllers/api/v1/outfits_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::OutfitsController < ApplicationController
+    def index
+      outfits = Outfit.all
+      render json: outfits
+      #render json: OutfitSerializer.new(@outfits)
+    end
+ 
+    def create
+      outfit = Outfit.new(outfit_params)
+      if outfit.save
+        render json: outfit, status: :accepted
+      else
+        render json: {errors: outfit.errors.full_messages}, status: unprocessible_entity
+      end
+    end
+ 
+    private
+ 
+    def outfit_params
+      params.require(:outfit).permit(:name, :description, :image_url, :weather_id)
+    end
+end
+
