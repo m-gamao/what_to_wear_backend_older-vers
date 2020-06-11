53d44dd4af311ecf6e4fbfba51d7f5cd39f7bd38
gigi <mmgamao1@gmail.com>
2020-06-10 17:09:40 -0700

    changed the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,43 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#Conditions:
-freezing = Condition.create(description: 'Freezing', temperature: '0-32 degrees');
-freezing/rainy = Condition.create(description: 'Freezing/Rainy', temperature: '0-32 degrees'); 
-
-cold = Condition.create(description: 'Cold', temperature: '33-50 degrees'); 
-cold/rainy = Condition.create(description: 'Cold/Rainy', temperature: '33-50 degrees'); 
-cold/snowing = Condition.create(description: 'Cold/Snowing', temperature: '33-50 degrees'); 
-
-cool = Condition.create(description: 'Cool', temperature: '51-69 degrees'); 
-cool/rainy = Condition.create(description: 'Cool/Rainy', temperature: '51-69 degrees'); 
-
-warm = Condition.create(description: 'Warm', temperature: '70-80 degrees'); 
-warm/rainy = Condition.create(description: 'Warm/Rainy', temperature: '70-80 degrees'); 
-
-hot = Condition.create(description: 'Hot', temperature: '81-95 degrees');
-hot/rainy = Condition.create(description: 'Hot/Rainy', temperature: '81-95 degrees');
-
-scorching = Condition.create(description: 'Scorching', temperature: '96-120 degrees'); 
-
-
-
+# OUTFITS:
   
-Outfit.create('freezing', description: 'winter coat, layers, pants, socks, boots, mittens, scarf')
-Outfit.create('freezing/rainy', description: 'waterproof winter coat, layers, pants, socks, rain boots, mittens, scarf, umbrella')
-Outfit.create('cold', description: 'winter/fall coat, sweater, pants, socks, boots/shoes, mittens or scarf')
-Outfit.create('cold/rainy', description: 'winter/fall coat or raincoat, sweater, pants, socks, rain boots, mittens, umbrella')
-Outfit.create('cold/snowy', description: 'winter coat, sweater, pants, socks, snow boots, mittens, scarf')
-Outfit.create('cool', description: 'light jacket, shirt, pants, flats/heels')
-Outfit.create('cool/rainy', description: 'raincoat, shirt, pants, rain boots')
-Outfit.create('warm', description: 't-shirt, shorts/skirt, sandals/heels')
-Outfit.create('warm/rainy', description: 't-shirt, shorts/skirt, rain boots')
-Outfit.create('hot temp', description: 't-shirt, shorts/skirt, sandals')
-Outfit.create('hot/rainy', description: 't-shirt, shorts/skirt, rain boots')
-Outfit.create('scorching temp', description: 'tank tops, shorts/skirt, sandals')
+Outfit.create('freezing', temperature: '0-32 degrees', 
+  precipitation: 'none', description: 'winter coat, layers, pants, socks, boots, mittens, scarf')
+
+Outfit.create('freezing/rainy', temperature: '0-32 degrees', precipitation: 'Rain', 
+  description: 'waterproof winter coat, layers, pants, socks, rain boots, mittens, scarf, umbrella')
+
+Outfit.create('cold', temperature: '33-50 degrees', 
+  precipitation: 'none', description: 'winter/fall coat, sweater, pants, socks, boots/shoes, mittens or scarf')
+
+Outfit.create('cold/rainy', temperature: '33-50 degrees', 
+  precipitation: 'Rain', description: 'winter/fall coat or raincoat, sweater, pants, socks, rain boots, mittens, umbrella')
+
+Outfit.create('cold/snowy', temperature: '33-50 degrees', 
+  precipitation: 'Snow', description: 'winter coat, sweater, pants, socks, snow boots, mittens, scarf')
+
+Outfit.create('cool', temperature: '51-69 degrees', 
+  description: 'light jacket, shirt, pants, flats/heels')
+
+Outfit.create('cool/rainy', temperature: '51-69 degrees', 
+  precipitation: 'Rain', description: 'raincoat, shirt, pants, rain boots')
+
+Outfit.create('warm', temperature: '70-80 degrees', 
+  precipitation: 'none', description: 't-shirt, shorts/skirt, sandals/heels')
+
+Outfit.create('warm/rainy', temperature: '70-80 degrees', 
+  precipitation: 'Rain', description: 't-shirt, shorts/skirt, rain boots')
+
+Outfit.create('hot temp', temperature: '81-95 degrees', 
+  precipitation: 'none', description: 't-shirt, shorts/skirt, sandals')
+
+Outfit.create('hot/rainy', temperature: '81-95 degrees', 
+  precipitation: 'Rain', description: 't-shirt, shorts/skirt, rain boots')
+
+Outfit.create('scorching temp', temperature: '96-120 degrees', 
+  precipitation: 'none', description: 'tank tops, shorts/skirt, sandals')
 
 
 
--- notes.md
+++ notes.md
@@ -24,4 +24,76 @@ calling in API:
 https://github.com/lostisland/faraday
 
 
+ZIPCODE.JS:
 
+STEP 1:  
+document.addEventListener('DOMContentLoaded', () => {
+  getZipcode()
+  const createOutfitForm = document.querySelector("#create-outfit-form")
+  createOutfitForm.addEventListener("submit", (e) => createFormHandler(e))
+})
+
+function createFormHandler(e) {
+  e.preventDefault()
+  const zipcodeInput = document.querySelector('#zipcode').value
+  let URL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcodeInput}&appid=529455d82ee4cf584b975c5961524222&units=imperial`
+
+  fetch(URL)
+  .then(r => r.json())
+  .then(json => {
+    let temperature=json.main.temp 
+    let condition=json.weather[0].main;  
+          //console.log(json.main.temp);
+          //console.log(json.weather[0].main);
+})
+ 
+
+
+
+
+#Conditions:
+# freezing = Condition.create(temperature: '0-32 degrees');
+# freezing/rainy = Condition.create(precipitation: 'Rain', temperature: '0-32 degrees'); 
+
+# cold = Condition.create(temperature: '33-50 degrees'); 
+# cold/rainy = Condition.create(precipitation: 'Rain', temperature: '33-50 degrees'); 
+# cold/snowing = Condition.create(precipitation: 'Snow', temperature: '33-50 degrees'); 
+
+# cool = Condition.create(temperature: '51-69 degrees'); 
+# cool/rainy = Condition.create(precipitation: 'Rain', temperature: '51-69 degrees'); 
+
+# warm = Condition.create(description: 'Warm', temperature: '70-80 degrees'); 
+# warm/rainy = Condition.create(precipitation: 'Rain', temperature: '70-80 degrees'); 
+
+# hot = Condition.create(description: 'Hot', temperature: '81-95 degrees');
+# hot/rainy = Condition.create(precipitation: 'Rain', temperature: '81-95 degrees');
+
+# scorching = Condition.create(temperature: '96-120 degrees'); 
+
+_______________________________________________________________________________
+
+TO ADD TO HTML FILE:
+
+function Outfits(description){
+    this.description = description
+  
+  Outfit.prototype.freezing = function(){
+    return ''winter coat, layers, pants, socks, boots, mittens, scarf''
+  }
+  
+  Outfit.prototype.freezingRainy = function(){
+    return 'waterproof winter coat, layers, pants, socks, rain boots, mittens, scarf, umbrella'
+  }
+  
+  Outfit.prototype.doCharity = function(){
+    return "I like to help people."
+  }
+  
+  Outfit.prototype.releasePressStatement = function(){
+    return "You will see great things from Scuber."
+  }
+  
+  Outfit.prototype.sayHi = function(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+  
